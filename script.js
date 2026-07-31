b39767883c4da96166ff55eaeb1e1b620fe9dc00
Darshan712006 <dhokiyadarshan008@gmail.com>
2026-07-31 21:01:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,25 @@ document.querySelectorAll(".nav-links a").forEach(link => {
         menuToggle.innerHTML = "☰";
     });
 });
+
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeLightbox = document.querySelector(".close-lightbox");
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+closeLightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});

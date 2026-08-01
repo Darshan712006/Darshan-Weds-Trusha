2a7218c8e46214bbc9f8150c939c3de80975b17e
Darshan712006 <dhokiyadarshan008@gmail.com>
2026-08-02 00:43:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,3 +96,24 @@ function changeBackground(){
 
 changeBackground();
 setInterval(changeBackground, 5000);
+
+const heartsContainer = document.getElementById("hearts-container");
+
+function createHeart(){
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+    heart.innerHTML = "❤";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = (4 + Math.random() * 4) + "s";
+    heart.style.fontSize = (15 + Math.random() * 20) + "px";
+
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 8000);
+}
+
+setInterval(createHeart, 700);

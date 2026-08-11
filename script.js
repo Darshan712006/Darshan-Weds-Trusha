828454ba9d62f9420b3d9738d16718cb2a817927
Darshan712006 <dhokiyadarshan008@gmail.com>
2026-08-11 20:25:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -93,30 +93,3 @@ function createHeart(){
 }
 
 setInterval(createHeart, 1200);
-
-const hiddenElements = document.querySelectorAll(".hidden");
-
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-hiddenElements.forEach(el=>{
-    observer.observe(el);
-});
-
-window.addEventListener("load", () => {
-    const loader = document.getElementById("loader");
-    if(loader){
-        loader.style.display = "none";
-    }
-});
-
-window.addEventListener("load", function () {
-    setTimeout(function () {
-        document.getElementById("preloader").classList.add("hide");
-    }, 2000);
-});
